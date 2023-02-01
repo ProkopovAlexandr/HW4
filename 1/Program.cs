c3630c7e56f890639d9d0a12cb12e3eb80752183
Alexandr <prok0107@mail.ru>
2023-02-01 13:48:49 +0300

    готовы задачи 1 и 2

---
+++ 1/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Число: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.Write("Степень: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int Pow(int num, int pow)
+{
+    int i = 1;
+    int result = 1;
+    while (i <= pow)
+    {
+        result = result * num;
+        i++;
+    }
+    return result;
+}
+
+Console.Write(Pow(x, n));
---
+++ 2/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.Write("Ведите число: ");
+string text = Console.ReadLine();
+int num = Convert.ToInt32(text);
+int sum = 0;
+int i = 0;
+
+
+while (i < text.Length)
+{
+    int dig = Convert.ToInt32(Convert.ToString(text[i]));
+    try
+    {
+        if ( num % dig == 0)
+        {
+            sum = sum + dig;
+        
+        }
+        i++;
+    }    
+    catch (Exception ex)
+    {i++;}
+}
+Console.Write("Сумма делителей: ");
+Console.Write(sum);
